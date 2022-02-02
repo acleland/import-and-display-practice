cec96b36da13ff37c4d609db77f036efa6add134
Andrea Cleland <andreaclelan@gmail.com>
2022-02-01 17:14:07 -0800

    added my countries to index.html

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 import { dinosaurs } from './dino-data.js';
 import { favoriteThings } from './favorite-things-data.js';
 import { foods } from './food-data.js';
-import { renderDino, renderListItem, renderFood } from './utils.js';
+import { countries } from './country-data.js';
+
+import { renderDino, renderListItem, renderFood, renderCountry } from './utils.js';
 
 const dinoContainer = document.getElementById('dinoContainer');
 const favoriteThingsList = document.getElementById('favoriteThings');
 const foodsContainer = document.getElementById('foodsContainer');
+const countriesContainer = document.getElementById('countriesContainer');
 
 for (let dino of dinosaurs) {
     dinoContainer.append(renderDino(dino));
@@ -20,6 +23,9 @@ for (let food of foods) {
     foodsContainer.append(renderFood(food));
 }
 
+for (let country of countries) {
+    countriesContainer.append(renderCountry(country));
+}
 
 // set event listeners 
   // get user input
